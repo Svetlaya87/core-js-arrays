365f10752cd49acc157ba62bc3e76401bd211925
Svetlaya87 <svetlaya87@gmail.com>
2024-01-12 22:10:11 +0200

    feat: solve getElementByIndices task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -446,8 +446,12 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  let res = arr;
+  return indices.reduce(function x(acc, el) {
+    res = res[el];
+    return res;
+  }, res);
 }
 
 /**
